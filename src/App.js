0bf9cbc34b27858c44611a6fdad8f21a1984e465
kalai0304 <kalaiyarasan0304@gmail.com>
2024-03-20 14:25:52 +0530

    given funcitonlity added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 function App() {
+  const [num, SetNum] = useState([]);
+
+  
+  const FindPrimeNumber = async () => {
+    try {
+      const res = await Axios.get("http://20.244.56.144/numbers/primes");
+      SetNum([...res.data.numbers]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const FindFibinocciNumber = async () => {
+    try {
+      const res = await Axios.get("http://20.244.56.144/numbers/fibo");
+      SetNum([...res.data.numbers]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const findEvenNumber = async () => {
+    try {
+      const res = await Axios.get("http://20.244.56.144/numbers/even");
+      SetNum([...res.data.numbers]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const findRandomNumber = async () => {
+    try {
+      const res = await Axios.get("http://20.244.56.144/numbers/rand");
+      SetNum([...res.data.numbers]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <h1>Average Calculator</h1>
+          <h3>Note:</h3>
+          <ul>
+          <li>P-Prime</li>
+          <li>F-Fibinocci</li>
+          <li>E-EVEN</li>
+          <li>R-Random </li>
+          </ul>
+        </div>
+        <div className="buttons">
+          <button className="btn" onClick={FindPrimeNumber}>P</button>
+          <button className="btn" onClick={FindFibinocciNumber}>F</button>
+          <button className="btn" onClick={findEvenNumber}>E</button>
+          <button className="btn" onClick={findRandomNumber}>R</button>
+        </div>
+        <div className="display ">
+          {num.map((result) => {
+            return (
+              <div>
+                {result}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
